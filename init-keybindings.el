269ab86229f62b59d8ef08a279db3ac3dee84346
0YX <34591111+0YX@users.noreply.github.com>
2019-02-03 21:25:51 +0800

    Delete init-keybindings.el

--- init-keybindings.el
+++
@@ -1,7 +0,0 @@
-(global-set-key "\C-x\ \C-r" 'recentf-open-files)
-(global-set-key (kbd "<f3>") 'open-my-init-file)
-(global-set-key "\C-x\ \C-r" 'recentf-open-files)
-
-
-
-(provide 'init-keybindings)
