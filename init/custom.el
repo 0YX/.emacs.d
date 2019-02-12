423ac225b746ab2bcbbe5fe8916a950ab6418401
0YX <34591111+0YX@users.noreply.github.com>
2019-02-12 14:37:22 +0800

    Add files via upload

---
+++ init/custom.el
@@ -0,0 +1,18 @@
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ac-etags-requires 1)
+ '(package-selected-packages
+   (quote
+    (pretty-mode yasnippet-snippets ycmd use-package-hydra use-package-ensure-system-package smartparens python-mode nyan-mode lsp-ui lsp-python jedi ivy-yasnippet irony-eldoc flycheck-pyflakes exec-path-from-shell elpy doom-themes doom-modeline dashboard company-lsp company-irony-c-headers company-irony company-anaconda cmake-mode cmake-ide beacon auto-complete-clang auto-compile ac-etags 0blayout))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(provide 'custom)
