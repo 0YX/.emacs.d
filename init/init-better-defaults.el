f3c53550e2c4a8ba766912f52d0c238e5d6f2c04
0YX <34591111+0YX@users.noreply.github.com>
2019-12-31 21:21:42 +0800

    Update init-better-defaults.el

--- init/init-better-defaults.el
+++ init/init-better-defaults.el
@@ -36,20 +36,8 @@
 
 
 
-
-;;set transparent effect
-(global-set-key [(f9)] 'loop-alpha)
-(setq alpha-list '((100 100) (95 65) (85 55) (75 45) (65 35)))  
-(defun loop-alpha ()  
-      (interactive)  
-      (let ((h (car alpha-list)))                ;; head value will set to  
-        ((lambda (a ab)  
-           (set-frame-parameter (selected-frame) 'alpha (list a ab))  
-           (add-to-list 'default-frame-alist (cons 'alpha (list a ab)))  
-           ) (car h) (car (cdr h)))  
-        (setq alpha-list (cdr (append alpha-list (list h))))  
-        )
-)
+;;set transparency
+ (add-to-list 'default-frame-alist '(alpha . (80 75)))
 
 ;;dashboard setting
 
